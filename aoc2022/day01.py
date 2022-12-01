2cac7cddae237e5be4e3042cfd295cb570ade88c
Tom Gothan <tom.gothan@googlemail.com>
2022-12-01 07:16:02 +0100

    aoc 2022 day01

---
+++ aoc2022/day01.py
@@ -0,0 +1,46 @@
+import os
+
+def getSumOfCalories(lines):
+    sumOfCalories = []
+
+    currentCalories = 0
+    for line in lines:
+        if line == "\n":
+            sumOfCalories.append(currentCalories)
+            currentCalories = 0
+        else:
+            currentCalories += int(line)
+    
+    return sumOfCalories
+
+def puzzleA(lines):
+    return max(getSumOfCalories(lines))
+
+
+def puzzleB(lines):
+    sumOfCalories = getSumOfCalories(lines)
+
+    first = max(sumOfCalories)
+    sumOfCalories.remove(first)
+    second = max(sumOfCalories)
+    sumOfCalories.remove(second)
+    third = max(sumOfCalories)
+
+    return first + second + third
+
+
+if __name__ == '__main__':
+    day = "01"
+    currentPath = os.path.dirname(__file__)
+    relPath = "input/input_{0}.txt".format(day)
+    absPath = os.path.join(currentPath, relPath)
+    
+    with open(absPath, encoding = 'utf-8') as file:
+        inputLines = file.readlines()
+
+    a = puzzleA(inputLines)
+    b = puzzleB(inputLines)
+    print(a)
+    print(b)
+    assert a == 74711
+    assert b == 209481
