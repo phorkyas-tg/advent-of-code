e337dda8b84630e429846b0222ee283e9323fa5d
Tom Gothan <tom.gothan@googlemail.com>
2022-12-06 06:18:38 +0100

    aoc 2022 day 6

---
+++ aoc2022/day06.py
@@ -0,0 +1,41 @@
+import os
+
+def puzzleA(lines):
+    line = lines[0].strip()
+    markerLenght = 4
+
+    for i in range(len(line) - (markerLenght - 1)):
+        marker = set(line[i:i+markerLenght])
+        if len(marker) == markerLenght:
+            return i + markerLenght
+
+    return 0
+
+
+def puzzleB(lines):
+    line = lines[0].strip()
+    markerLenght = 14
+
+    for i in range(len(line) - (markerLenght - 1)):
+        marker = set(line[i:i+markerLenght])
+        if len(marker) == markerLenght:
+            return i + markerLenght
+
+    return 0
+
+
+if __name__ == '__main__':
+    day = "06"
+    currentPath = os.path.dirname(__file__)
+    relPath = "input/input_{0}.txt".format(day)
+    absPath = os.path.join(currentPath, relPath)
+    
+    with open(absPath, encoding = 'utf-8') as file:
+        inputLines = file.readlines()
+
+    a = puzzleA(inputLines)
+    b = puzzleB(inputLines)
+    print(a)
+    print(b)
+    assert a == 1987
+    assert b == 3059
